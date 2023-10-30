9a2505997f3a5a647936acb8935a839df89be034
dzarkV <dfvargasp@unal.edu.co>
2023-10-30 13:40:54 -0500

    question1 modified to new city assert in test

--- part1/question1.py
+++ part1/question1.py
@@ -16,23 +16,24 @@
 def get_city_temperature(city_searched: str) -> int|str:
    """Returns the temperature for a given city."""
     
-   cities_and_temperatures = {city["name"]:city["temperature"] for city in cities}
-   if city_searched in cities_and_temperatures:
-      return cities_and_temperatures.get(city_searched)
-   else:
-      return "uncertain temperature"
+   cities_and_temperatures: dict = {city["name"]:city["temperature"] for city in cities}
+   return cities_and_temperatures.get(city_searched)
 
 
 def get_city_weather(city: str) -> str:
    """Returns the weather for a given city."""
-   sky_condition = None
+   
+   sky_condition: str = None
+   temperature: int = None
 
    for city_data in cities:
       if city_data["name"] == city:
-         sky_condition = city_data.get("sky_condition", "unknown")
+         sky_condition = city_data.get("sky_condition", "unknown conditions")
+         temperature = get_city_temperature(city)
          break
-
-   temperature = get_city_temperature(city)
+      else:
+         sky_condition = "uncertain conditions"
+         temperature = "uncertain temperature"
 
    return str(temperature) + " degrees and " + sky_condition
 
@@ -42,5 +43,5 @@ cities = [
     {"name": "Quito", "temperature": 22, "sky_condition": "sunny"},
     {"name": "Sao Paulo", "temperature": 17, "sky_condition": "cloudy"},
     {"name": "San Francisco", "temperature": 16},
-    {"name": "New York", "temperature": 14, "sky_condition": "rainy"},
+    {"name": "New York", "temperature": 14, "sky_condition": "rainy"}
 ]
--- part1/question1_test.py
+++ part1/question1_test.py
@@ -5,3 +5,7 @@ def test_get_city_weather():
   assert get_city_weather("Quito") == "22 degrees and sunny"
 
   assert get_city_weather("New York") == "14 degrees and rainy"
+
+  assert get_city_weather("San Francisco") == "16 degrees and unknown conditions"
+
+  assert get_city_weather("Bogotá") == "uncertain temperature degrees and uncertain conditions"
