43daff577526578451ac9fee2c288c046bb6f7ba
dzarkV <dfvargasp@unal.edu.co>
2023-10-30 16:39:28 -0500

    formating question3 files

--- part1/oven.py
+++ part1/oven.py
@@ -1,24 +1,45 @@
 class Oven:
+  """ Magical oven that can combine ingredients at 
+  different temperatures to craft special materials. """
+  
   def __init__(self):
     self.ingredients: list = []
     self.output: str = None
 
+
   def add(self, item):
+    """ Add an ingredient to be combined """
     self.ingredients.append(item)
 
+
   def freeze(self):
+    """ Freeze the ingredients """
     self.output = "snow"
 
+
   def boil(self, temperature: float = 100):
-    if "lead" in self.ingredients and "mercury" in self.ingredients and temperature >= 5000:
-        self.output = "gold"
-    elif "cheese" in self.ingredients and "dough" in self.ingredients and "tomato" in self.ingredients:
-        self.output = "pizza"
+    """ Boil the ingredients """
+    if (
+          "lead" in self.ingredients
+          and "mercury" in self.ingredients
+          and temperature >= 5000
+      ):
+          self.output = "gold"
+    elif (
+          "cheese" in self.ingredients
+          and "dough" in self.ingredients
+          and "tomato" in self.ingredients
+      ):
+          self.output = "pizza"
     else:
-        self.output = "boiled " + ", ".join(self.ingredients)
+          self.output = "boiled " + ", ".join(self.ingredients)
+
 
   def wait(self):
+    """ Combine the ingredients with no change in temperature """
     self.output = "mixing " + ", ".join(self.ingredients)
 
+
   def get_output(self):
+    """ Get the result of crafting materials"""
     return self.output
--- part1/question3_test.py
+++ part1/question3_test.py
@@ -1,33 +1,19 @@
 from question3 import alchemy_combine, make_oven
 
+
 def test_alchemy_combine():
+    assert alchemy_combine(make_oven(), ["lead", "mercury"], 5000) == "gold"
 
-  assert alchemy_combine(
-    make_oven(),
-    ["lead", "mercury"],
-    5000
-  ) == "gold"
+    assert alchemy_combine(make_oven(), ["water", "air"], -100) == "snow"
 
-  assert alchemy_combine(
-    make_oven(),
-    ["water", "air"],
-    -100
-  ) == "snow"
+    assert alchemy_combine(make_oven(), ["cheese", "dough", "tomato"], 150) == "pizza"
 
-  assert alchemy_combine(
-    make_oven(),
-    ["cheese", "dough", "tomato"],
-    150
-  ) == "pizza"
+    assert (
+        alchemy_combine(make_oven(), ["beans", "pig foot", "salt"], 60)
+        == "mixing beans, pig foot, salt"
+    )
 
-  assert alchemy_combine(
-    make_oven(),
-    ["beans", "pig foot", "salt"],
-    60
-  ) == "mixing beans, pig foot, salt"
-
-  assert alchemy_combine(
-    make_oven(),
-    ["beans", "pig foot", "salt"],
-    120
-  ) == "boiled beans, pig foot, salt"
+    assert (
+        alchemy_combine(make_oven(), ["beans", "pig foot", "salt"], 120)
+        == "boiled beans, pig foot, salt"
+    )
