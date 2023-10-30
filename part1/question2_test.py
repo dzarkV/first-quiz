220f17193c4f03d4d369102251637d97edb6e4a4
dzarkV <dfvargasp@unal.edu.co>
2023-10-30 14:38:58 -0500

    new test in question2 to check other datatypes

--- part1/question2_test.py
+++ part1/question2_test.py
@@ -8,3 +8,7 @@ def test_run_swapper():
   assert run_swapper(
     [ (1, 1), ("foo", "bar"), (13, "cows"), (None, "Some") ]
   ) == [ (1, 1), ("bar", "foo"), ("cows", 13), ("Some", None) ]
+
+  assert run_swapper(
+    [ ("", 0), ({"foo": "bar"}, ["bar", "foo"]), ([], 25.2045), (None, {3,2,1}) ]
+  ) == [ (0, ""), (["bar", "foo"], {"foo": "bar"}), (25.2045, []), ({3,2,1}, None) ]
